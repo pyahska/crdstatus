63023f7d1dd07c9a9ef01556ac92318490c519e4
pyahska <47941702+pyahska@users.noreply.github.com>
2019-03-24 15:49:00 +0530

    Create getstatus.py
    Server

---
+++ getstatus.py
@@ -0,0 +1,19 @@
+import requests
+import json
+from flask import Flask, request
+
+app = Flask(__name__)
+hdr = { 'Content-Type': 'application/json' }
+
+@app.route('/status', methods=['POST'])
+def returnstatus():
+    try:
+        reqjson = json.loads(request.data)
+        urn = reqjson["urn"]
+    except:
+        return("Invalid Request")
+    resp = requests.post('https://www.firstdatamerchantservices.com/MerchantOnBoarding/inquiryService', headers=hdr, json={"urn": urn})
+    return(resp.content)
+
+if __name__ == '__main__':
+    app.run(debug=True)
